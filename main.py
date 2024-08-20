2a77c64c7d6c65d85f577d8e5d16c3951b2804fe
Lighting123 <emmanuelekpar@gmail.com>
2024-08-20 15:56:19 +0100

    Draw Snake On Screen

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# pygame setup
+pygame.init()
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+running = True
 
+# constants
+DIRECTION_LEFT = 0
+DIRECTION_RIGHT = 1
+DIRECTION_UP = 3
+DIRECTION_DOWN = 4
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+size = 1
 
+def draw_snake(direction):
+    snake = pygame.Surface((50, 50))
+    snake.fill('lightgreen')
+    snake_rect_list = []
+    for i in range(size):
+        if len(snake_rect_list) == 0:
+            snake_rect_list.append(pygame.draw.rect(snake, 'darkgreen', (0, 0, 8, 6)))
+        else:
+            last_snake_rect = snake_rect_list[-1]
+            snake_rect_list.append(pygame.draw.rect(snake, 'darkgreen', (last_snake_rect.left+8, 0, 8, 6)))
+    snake_rect_list.append(pygame.draw.rect(snake, 'darkgreen', (snake_rect_list[-1].left+8, 0, 8, 6)))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    return snake
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Render game
+    screen.fill('lightgreen')
+    screen.blit(draw_snake(DIRECTION_LEFT), (0, 0))
+
+    pygame.display.update()
+
+    # limit fps to 60
+    clock.tick(60)
+
+pygame.quit()
